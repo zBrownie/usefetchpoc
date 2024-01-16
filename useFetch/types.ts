665758320f276a5ab283783c8ed7d1e6a420d0ad
bzenerato <bru.nomoraes@hotmail.com>
2024-01-15 21:23:20 -0300

    feat: callApi with promise return

--- useFetch/types.ts
+++ useFetch/types.ts
@@ -1,11 +1,18 @@
+import { ApiPropsReturn } from "../../Api/fetch";
+
 export type TStatus = "idle" | "loading" | "error";
 export type TError = string | null;
 export type TData<T> = T | null;
-
-export type TCallApi = (secondUrl?: string) => Promise<void>;
 
 export interface UseFetchGetReturn<T = object> {
   status: TStatus;
   error: string | null;
   data: T | null;
 }
+
+export type THookReturn<T> = {
+  callApi: (secondUrl?: string) => Promise<ApiPropsReturn<T> | undefined>;
+  data: TData<T>;
+  error: TError;
+  status: TStatus;
+};
